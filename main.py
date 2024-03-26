39cc69da1f6c81cd0d007d4e7abfd21688c0f40a
PavelStarun <starunpavelpython@gmail.com>
2024-03-26 21:01:01 +0300

    new level 2 run

--- main.py
+++ main.py
@@ -6,35 +6,39 @@ import tkinter as tk
 def start_game():
     root.destroy()
 
+
 def quit_game():
     root.destroy()
     pygame.display.quit()
 
+
 def reset_game():
-    global start_ticks, hits, target_x, target_y, game_active
+    global start_ticks, hits, target_x, target_y, game_active, target_speed, target_direction
     start_ticks = pygame.time.get_ticks()
     hits = 0
     target_x = random.randint(0, SCREEN_WIDTH - target_width)
     target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     game_active = True
+    if level == 2:
+        target_speed_x = random.choice([-2, 2])  # Скорость движения мишени по X
+        target_speed_y = random.choice([-2, 2])  # Скорость движения мишени по Y
+    else:
+        target_speed_x = 0.4
+        target_speed_y = 0.3
+
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("img/apex.jpg")
-pygame.display.set_icon(icon)
 
 target_image = pygame.image.load("img/mishen.png")
-target_width = 80
-target_height = 80
-target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-
+target_width, target_height = 80, 80
 color = (76, 81, 74)
-
+font = pygame.font.Font(None, 36)
+button_font = pygame.font.Font(None, 26)
+level = 1
 
 root = tk.Tk()
 root.title("Приветствую тебя в моей первой игре!")
@@ -53,10 +57,10 @@ button2.place(relx=0.32, rely=0.8, height=35, width=100)
 
 root.mainloop()
 
-font = pygame.font.Font(None, 36)
-button_font = pygame.font.Font(None, 26)
 game_time = 10
 reset_game()
+target_speed_x = 0.5
+target_speed_y = 0.5
 
 running = True
 while running:
@@ -67,34 +71,50 @@ while running:
     timer_text = font.render(f"Time Left: {time_left}s", True, (255, 255, 255))
     screen.blit(timer_text, (10, 10))
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if game_active and event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
-                hits += 1
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-        elif not game_active and event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            if retry_button.collidepoint(mouse_x, mouse_y):
-                reset_game()
-
     if game_active:
+        if game_active and level == 2:
+            target_x += target_speed_x
+            target_y += target_speed_y
+            # Изменение направления при достижении границ экрана
+            if target_x <= 0 or target_x >= SCREEN_WIDTH - target_width:
+                target_speed_x *= -0.4
+            if target_y <= 0 or target_y >= SCREEN_HEIGHT - target_height:
+                target_speed_y *= -0.4
         screen.blit(target_image, (target_x, target_y))
+        if time_left == 0:
+            game_active = False
     else:
         result_text = font.render(f"Количество попаданий: {hits}", True, (255, 255, 255))
-        screen.blit(result_text, (
-        SCREEN_WIDTH // 2 - result_text.get_width() // 2, SCREEN_HEIGHT // 2 - result_text.get_height() // 2))
+        screen.blit(result_text, (SCREEN_WIDTH // 2 - result_text.get_width() // 2, SCREEN_HEIGHT // 2 - 20))
+
         retry_button = pygame.Rect(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 50, 200, 40)
         pygame.draw.rect(screen, (0, 255, 0), retry_button)
         retry_text = button_font.render("Попробуй еще раз", True, (255, 255, 255))
-        screen.blit(retry_text, (SCREEN_WIDTH // 2 - retry_text.get_width() // 2, SCREEN_HEIGHT // 2 + 50 + 10))
+        screen.blit(retry_text, retry_button.topleft + pygame.math.Vector2(10, 10))
+
+        level2_button = pygame.Rect(SCREEN_WIDTH // 2 - 100, SCREEN_HEIGHT // 2 + 100, 200, 40)
+        pygame.draw.rect(screen, (0, 0, 255),         level2_button)
+        level2_text = button_font.render("Уровень 2", True, (255, 255, 255))
+        screen.blit(level2_text, level2_button.topleft + pygame.math.Vector2(10, 10))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if game_active:
+                if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
+                    hits += 1
+                    target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                    target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+            elif retry_button.collidepoint(mouse_x, mouse_y):
+                level = 1
+                reset_game()
+            elif level2_button.collidepoint(mouse_x, mouse_y) and not game_active:
+                level = 2
+                reset_game()
 
     pygame.display.update()
 
-    if time_left <= 0:
-        game_active = False
-
 pygame.quit()
+
