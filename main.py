974881a2c264ca45420a4f78de2e53a3a952eaec
PavelStarun <starunpavelpython@gmail.com>
2024-03-27 12:33:15 +0300

    add music

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import random
 import tkinter as tk
-
+import os
 
 def start_game():
     root.destroy()
@@ -20,30 +20,41 @@ def reset_game():
     target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     game_active = True
     if level == 2:
-        target_speed_x = random.random  # Скорость движения мишени по X
-        target_speed_y = random.random  # Скорость движения мишени по Y
+        target_speed_x = random.random
+        target_speed_y = random.random
     else:
         target_speed_x = 0
         target_speed_y = 0
 
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (100, 100)
 
 pygame.init()
+
+pygame.mixer.music.load("music/fon.ogg")
+pygame.mixer.music.play(-1)
+pygame.mixer.music.set_volume(0.18)
+
+hit_sound = pygame.mixer.Sound("music/shot.ogg")
+hit_sound.set_volume(0.05)
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Игра Тир")
-
 target_image = pygame.image.load("img/mishen.png")
 target_width, target_height = 80, 80
 color = (76, 81, 74)
 font = pygame.font.Font(None, 36)
 button_font = pygame.font.Font(None, 26)
-level = 1
+
 
 root = tk.Tk()
-root.title("Приветствую тебя в моей первой игре!")
+root.title("Приветствую тебя в моей первой игре! ИГРА - ТИР")
 root.configure(bg="light blue")
-root.geometry("470x270")
+width_tk = 470
+height_tk = 270
+center_x = 100 + (SCREEN_WIDTH - width_tk) // 2
+center_y = 100 + (SCREEN_HEIGHT - height_tk) // 2
+root.geometry(f'{width_tk}x{height_tk}+{center_x}+{center_y}')
 
 tk_img = tk.PhotoImage(file="img/tkimg2.png")
 tk_label = tk.Label(root, image=tk_img)
@@ -57,10 +68,13 @@ button2.place(relx=0.32, rely=0.8, height=35, width=100)
 
 root.mainloop()
 
+
+level = 1
 game_time = 10
 reset_game()
-target_speed_x = float(0.3)
-target_speed_y = float(0.2)
+target_speed_x = float(0.15)
+target_speed_y = float(0.15)
+
 
 running = True
 while running:
@@ -75,7 +89,6 @@ while running:
         if game_active and level == 2:
             target_x += target_speed_x
             target_y += target_speed_y
-            # Изменение направления при достижении границ экрана
             if target_x <= 0 or target_x >= SCREEN_WIDTH - target_width:
                 target_speed_x *= -1
             if target_y <= 0 or target_y >= SCREEN_HEIGHT - target_height:
@@ -105,6 +118,7 @@ while running:
             if game_active:
                 if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
                     hits += 1
+                    hit_sound.play()
                     target_x = random.randint(0, SCREEN_WIDTH - target_width)
                     target_y = random.randint(0, SCREEN_HEIGHT - target_height)
             elif retry_button.collidepoint(mouse_x, mouse_y):
